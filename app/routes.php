ce47252be4783f066694d73862c0f0ba34623746
Nathan Bunney <nathan@Inventaweb.net>
2015-01-24 00:20:52 -0800

    Updated routes

--- app/routes.php
+++ app/routes.php
@@ -4,7 +4,6 @@ App::post('/login', function() use($app) {
   $vars = json_decode($app->request->getBody());
 
   $filter = ['email'=>$vars->email, 'password'=>sha1($vars->password)];
-
   $reply = DB::table('user')->filter($filter)->run()->toNative();
 
   if (!empty($reply)){
@@ -17,6 +16,53 @@ App::post('/login', function() use($app) {
     ));
   }
 
+});
+
+App::post('/register', function() {
+
+  // Get the user
+  $user = json_decode(Request::getBody(), true);
+
+  // Hash the password
+  $password = sha1($user['password']);
+
+  // Make sure the passwords match
+  if($user['password'] != $user['passwordConfirm']) {
+
+    // If not, return a validation error
+    $code = 422;
+    $response = ['msg' => 'Passwords must match', 'error' => true];
+  } else {
+
+    // Set the password to the hash
+    $user['password'] = $password;
+
+    // Unset the confirm
+    unset($user['passwordConfirm']);
+
+    // Make sure the email is unique
+    $emailExists = DB::table('user')->filter(['email' => $user['email']])->run()->toNative();
+
+    // If it isn't, return a 403
+    if(empty($emailExists)) {
+
+      // Otherwise insert it and return if possible
+      try {
+        $newEmail = DB::table('user')->insert($user);
+        $code = 200;
+        $response = ['msg' => 'User created', 'data' => $newEmail];
+      } catch(Exception $e){
+        $code = 500;
+        $response = ['msg' => 'There was an error registering your user. Please try again'];
+      }
+
+    } else {
+      $code = 403;
+      $response = ['msg' => 'There was a problem with your email or password. Please check your email/password and try again.'];
+    }
+  }
+
+  App::render($code, $response);
 });
 
 App::post('/register', function() {
