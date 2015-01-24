86e08832886077f906bfda1045cf24cae4d02827
Nathan Bunney <nathan@Inventaweb.net>
2015-01-23 23:37:53 -0800

    Build DB File

---
+++ generateDB.php
@@ -0,0 +1,32 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_errors', 1);
+
+require __DIR__.'/vendor/autoload.php';
+
+$config = require __DIR__.'/app/configuration.php';
+
+$conn = r\connect($config['RETHINK_HOST']);
+$conn->useDb($config['RETHINK_DATABASE']);
+
+switch($argv[1]){
+  case ('users'):
+    // Create a test table
+
+    try{
+      r\db("traintracker")->tableDrop("user")->run($conn);
+    }catch(Exception $e){}
+
+    r\db("traintracker")->tableCreate("user")->run($conn);
+
+    $spass = password_hash('1234', PASSWORD_BCRYPT);
+
+    $user1 = array('uname' => "testuser", "password" => $spass, "email"=> "test@user.com");
+    $user2 = array('uname' => "testuser2", "password" => $spass, "email"=> "test@user2.com");
+
+    r\db("traintracker")->table('user')->insert($user1)->run($conn);
+    r\db("traintracker")->table('user')->insert($user2)->run($conn);
+
+    break;
+}
