78fe6990d1716aec94b38192e237c8ea7a3f767d
Matthew Davies <daviesgeek@icloud.com>
2015-01-24 00:32:04 -0800

    Removed a bunch of duplicate routes (huhhhh???) Fixed some issues with registration

--- app/routes.php
+++ app/routes.php
@@ -10,7 +10,7 @@ App::post('/login', function() use($app) {
     App::render(200,array(
       'msg' => 'Logged In'
     ));
-  }else{
+  } else {
     App::render(403,array(
       'msg' => 'Not Logged In'
     ));
@@ -22,11 +22,10 @@ App::post('/register', function() {
 
   // Get the user
   $user = json_decode(Request::getBody(), true);
-
   // Hash the password
   $password = sha1($user['password']);
 
-  // Make sure the passwords match
+  // // Make sure the passwords match
   if($user['password'] != $user['passwordConfirm']) {
 
     // If not, return a validation error
@@ -48,103 +47,10 @@ App::post('/register', function() {
 
       // Otherwise insert it and return if possible
       try {
-        $newEmail = DB::table('user')->insert($user);
+        $user['id'] = DB::table('user')->insert($user)->run()->toNative();
         $code = 200;
-        $response = ['msg' => 'User created', 'data' => $newEmail];
-      } catch(Exception $e){
-        $code = 500;
-        $response = ['msg' => 'There was an error registering your user. Please try again'];
-      }
-
-    } else {
-      $code = 403;
-      $response = ['msg' => 'There was a problem with your email or password. Please check your email/password and try again.'];
-    }
-  }
-
-  App::render($code, $response);
-});
-
-App::post('/register', function() {
-
-  // Get the user
-  $user = json_decode(Request::getBody(), true);
-
-  // Hash the password
-  $password = sha1($user['password']);
-
-  // Make sure the passwords match
-  if($user['password'] != $user['passwordConfirm']) {
-
-    // If not, return a validation error
-    $code = 422;
-    $response = ['msg' => 'Passwords must match', 'error' => true];
-  } else {
-
-    // Set the password to the hash
-    $user['password'] = $password;
-
-    // Unset the confirm
-    unset($user['passwordConfirm']);
-
-    // Make sure the email is unique
-    $emailExists = DB::table('user')->filter(['email' => $user['email']])->run()->toNative();
-
-    // If it isn't, return a 403
-    if(empty($emailExists)) {
-
-      // Otherwise insert it and return if possible
-      try {
-        $newEmail = DB::table('user')->insert($user);
-        $code = 200;
-        $response = ['msg' => 'User created', 'data' => $newEmail];
-      } catch(Exception $e){
-        $code = 500;
-        $response = ['msg' => 'There was an error registering your user. Please try again'];
-      }
-
-    } else {
-      $code = 403;
-      $response = ['msg' => 'There was a problem with your email or password. Please check your email/password and try again.'];
-    }
-  }
-
-  App::render($code, $response);
-});
-
-App::post('/register', function() {
-
-  // Get the user
-  $user = json_decode(Request::getBody(), true);
-
-  // Hash the password
-  $password = sha1($user['password']);
-
-  // Make sure the passwords match
-  if($user['password'] != $user['passwordConfirm']) {
-
-    // If not, return a validation error
-    $code = 422;
-    $response = ['msg' => 'Passwords must match', 'error' => true];
-  } else {
-
-    // Set the password to the hash
-    $user['password'] = $password;
-
-    // Unset the confirm
-    unset($user['passwordConfirm']);
-
-    // Make sure the email is unique
-    $emailExists = DB::table('user')->filter(['email' => $user['email']])->run()->toNative();
-
-    // If it isn't, return a 403
-    if(empty($emailExists)) {
-
-      // Otherwise insert it and return if possible
-      try {
-        $newEmail = DB::table('user')->insert($user);
-        $code = 200;
-        $response = ['msg' => 'User created', 'data' => $newEmail];
+        unset($user['password']);
+        $response = ['msg' => 'User created', 'data' => $user];
       } catch(Exception $e){
         $code = 500;
         $response = ['msg' => 'There was an error registering your user. Please try again'];
